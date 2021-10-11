d9e07dadade1ec03ada736880acb275dbc094dac
Recai-Capkin <recai.capkin28@gmail.com>
2021-10-11 16:06:16 +0300

    Model migration eklendi

--- kayitsistemi2/Migrations/20211011130523_ModelGuncelle.Designer.cs
+++ kayitsistemi2/Migrations/20211011130523_ModelGuncelle.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using kayitsistemi2.Data;
 
 namespace kayitsistemi2.Migrations
 {
     [DbContext(typeof(KayitdbContext))]
-    partial class KayitdbContextModelSnapshot : ModelSnapshot
+    [Migration("20211011130523_ModelGuncelle")]
+    partial class ModelGuncelle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
